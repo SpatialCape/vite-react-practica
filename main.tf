658b46826e091dc69c9d30c0e6c623ebe012d073
Jesus Donato <al22020310@itsa.edu.mx>
2026-02-19 11:49:39 -0600

    Cambio Prueba CI/CD

--- main.tf
+++ main.tf
@@ -9,9 +9,12 @@ variable "github_token" {
 }
 
 resource "aws_amplify_app" "hola_mundo" {
-  name       = "hola-mundo-vite"
-  repository = "https://github.com/javiercl/vite-hola-mundo.git"
-  oauth_token = var.github_token
+  name       = "vite-react-practica"
+  # 1. Actualizado con la URL de tu nuevo repositorio
+  repository = "https://github.com/SpatialCape/vite-react-practica.git"
+  
+  # 2. Cambiado de oauth_token a access_token (oauth_token está deprecado en AWS provider)
+  access_token = var.github_token
 
   build_spec = <<-EOT
 version: 1
@@ -39,7 +42,15 @@ EOT
   }
 }
 
-resource "aws_amplify_branch" "master" {
-  app_id      = aws_amplify_app.hola_mundo.id
-  branch_name = "master"
+# 3. Cambiamos el nombre del recurso y la rama a "main"
+resource "aws_amplify_branch" "main" {
+  app_id            = aws_amplify_app.hola_mundo.id
+  branch_name       = "main"
+  # 4. Habilitamos la construcción automática para cumplir con el Paso 8
+  enable_auto_build = true 
+}
+
+# Añadimos un output para que la terminal te regrese la URL de tu app al terminar
+output "amplify_app_url" {
+  value = "https://${aws_amplify_branch.main.branch_name}.${aws_amplify_app.hola_mundo.default_domain}"
 }
